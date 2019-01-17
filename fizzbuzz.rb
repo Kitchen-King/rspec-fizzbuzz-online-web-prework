40aa6e94aa22cff317a22cea5dc4b6d0f4d7e9ac
Kyle Spencer <spencerk2@vcu.edu>
2019-01-17 20:03:45 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(x)
-  if x % 3 == 0
-    return "fizz"
+  if x % 3 == 0 && x % 5 == 0
+    return "fizzbuzz"
   elsif x % 5 == 0
     return "buzz"
-  elsif x % 3 == 0 && x % 5 == 0
-    return "fizzbuzz"
+  elsif  x % 3 == 0
+    return "fizz"
   end
 end
